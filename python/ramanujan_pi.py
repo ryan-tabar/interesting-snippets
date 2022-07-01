dfd1e3d75fcbfb969407d9f619958b17706e229b
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2022-07-01 21:05:46 +0100

    first commit

---
+++ python/ramanujan_pi.py
@@ -0,0 +1,17 @@
+# Ramanujan's pi formula
+# https://www.cronodon.com/Programming/Pi.html
+
+import math as m
+
+def summation(N):
+    total = 0
+    for n in range(N):
+        numerator = m.factorial(4 * n) * (1103 + 26390 * n)
+        denominator = m.pow(m.factorial(n), 4) * m.pow(396, 4 * n)
+        total += numerator / denominator
+    return total
+
+constant = 2 * m.sqrt(2) / 9801
+result = constant * summation(20)
+result = 1 / result
+print(result)
