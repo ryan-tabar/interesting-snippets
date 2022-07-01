68eab9f7d77c4ed83eced15eef600f009e182680
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2022-07-01 10:28:19 +0100

    first commit

---
+++ python/prisoners.py
@@ -0,0 +1,27 @@
+# 100 prisoners numbered 1-100
+# 100 boxes with a piece of paper with its number
+# shuffle pieces of paper amongs the boxes
+# prisoners enter and have 50 tries to find their number
+# this is a strategy that gives a 31% chance of success
+
+import random
+
+SIMULATIONS = 10000
+PRISONERS = 100
+TRIES = 50
+
+successes = 0
+for _ in range(SIMULATIONS):
+    boxes = random.sample(range(PRISONERS), PRISONERS)
+    for prisoner in range(PRISONERS):
+        box = boxes[prisoner]
+        checked = set()
+        while box != prisoner and box not in checked:
+            checked.add(box)
+            box = boxes[box]
+        if len(checked) > TRIES:
+            break
+    else:
+        successes += 1
+
+print(f"Success rate: {successes/SIMULATIONS:.2%}")
