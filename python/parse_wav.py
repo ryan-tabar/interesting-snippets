109b590e3ec8a50e4bfd9c01274e31fea475b31f
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2022-10-09 20:08:47 +0100

    added sanity check for file size

--- python/parse_wav.py
+++ python/parse_wav.py
@@ -1,8 +1,11 @@
 # parsing wav data
 try:
-    with open("coin_roll.wav", "rb") as f:
+    with open("one_small_step.wav", "rb") as f:
+        file_size = 0
+        data_size = 0
+        # Total length of header should be 44 bytes
         print(f"File Format..........{f.read(4).decode('ascii')}")
-        print(f"File Size............{int.from_bytes(f.read(4), 'little', signed=False)}")
+        print(f"File Size............{(file_size := int.from_bytes(f.read(4), 'little', signed=False))}")
         print(f"File Type Header.....{f.read(4).decode('ascii')}")
         print(f"Format Chunk Marker..{f.read(4).decode('ascii')}")
         print(f"Length of Format.....{int.from_bytes(f.read(4), 'little', signed=False)}")
@@ -13,7 +16,14 @@ try:
         print(f"(BPS * C / 8)........{int.from_bytes(f.read(2), 'little', signed=False)}")
         print(f"Bits Per Sample......{int.from_bytes(f.read(2), 'little', signed=False)}")
         print(f"'data' chunk header..{f.read(4).decode('ascii')}")
-        print(f"File Size (data).....{int.from_bytes(f.read(4), 'little', signed=False)}")
+        print(f"File Size (data).....{(data_size := int.from_bytes(f.read(4), 'little', signed=False))}")
 
-except IOError:
-    print("Error While Openning the file!")
+        # sanity check
+        if (file_size - data_size != 36):
+            raise ValueError("Incorrect file or data size")
+
+except OSError as err:
+    print(f"OS Error: {err}")
+
+except ValueError as err:
+    print(f"Value Error: {err}")
