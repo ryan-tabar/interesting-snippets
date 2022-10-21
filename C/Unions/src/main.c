fcd21bb13131b6e964c5d3302ffb77beb68362f4
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2022-10-21 10:12:31 +0100

    tidied struct and prints

--- C/Unions/src/main.c
+++ C/Unions/src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-union Register {
-    struct {
+// An example of using a union to represent an 8-bit register
+union Register 
+{
+    struct 
+    {
         unsigned char b0: 1;
         unsigned char b1: 1;
         unsigned char b2: 1;
@@ -11,25 +14,30 @@ union Register {
         unsigned char b6: 1;
         unsigned char b7: 1;
     } bits;
-    unsigned char buffer;
+    unsigned char value;
 } reg;
 
 int main()
 {
-    reg.buffer = 24;
+    reg.value = 24;
 
-    printf("value of reg: %d, size of reg.bits in bytes: %d\n", reg.buffer, sizeof(reg.bits));
+    printf("value:  %d, size of reg.bits in bytes:  %d\n", 
+    reg.value, 
+    sizeof(reg.bits));
 
-    // manipulate individual bits
+    // manipulate individual bits (little endian)
     reg.bits.b0 = 1;
     reg.bits.b1 = 0;
     reg.bits.b2 = 1;
     reg.bits.b3 = 1;
-    reg.bits.b5 = 1;
-    reg.bits.b6 = 1;
-    reg.bits.b7 = 0;
+    reg.bits.b4 = 1;
+    reg.bits.b5 = 0;
+    reg.bits.b6 = 0;
+    reg.bits.b7 = 1;
 
-    printf("value of buffer: %d size of reg.buffer in bytes: %d\n", reg.buffer, sizeof(reg.buffer));
+    printf("value: %d, size of reg.value in bytes: %d\n", 
+    reg.value, 
+    sizeof(reg.value));
         
     return 0;
 }
